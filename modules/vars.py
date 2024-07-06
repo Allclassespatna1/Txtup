2f4293781a002bb3f634a730d85ddcf8c95efecc
Allclassespatna1 <167908825+Allclassespatna1@users.noreply.github.com>
2024-07-06 20:57:32 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "21537501"
 api_hash = "02d8ef0eae2926ec4fd0cbff05ec0737"
-bot_token = "7397836592:AAGlY3MCUwfV5VCW5YE7fYkEoyxNvjEu-W0"
+bot_token = "7231178831:AAECrLSWZ-nl-HWBqDNpIAQqWxfmT-utuHo"
