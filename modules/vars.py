7083ca4d1dda92cbd57ca484c2c60559f13d659f
Allclassespatna1 <167908825+Allclassespatna1@users.noreply.github.com>
2024-07-22 22:11:20 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "21537501"
 api_hash = "02d8ef0eae2926ec4fd0cbff05ec0737"
-bot_token = "7035841144:AAEMzVbXIE7xld2ycGvwVW1Nkn5ejT-8juo"
+bot_token = "7451364068:AAHuhmL_f1XsLkMOSIIJSeGCIVB7_fqmicM"
